0fd2eff9a6db367d4324b2c50ef93208c50d376d
6Ipkri <boonyaporn.sudjunham@kkumail.com>
2020-02-11 16:34:22 +0700

    sort activityReport by endDate

--- api/routes/operationRoutes.js
+++ api/routes/operationRoutes.js
@@ -252,6 +252,7 @@ router.post("/harvested/:landid", (req, res, next) => {
               }
             }
             logs.activities = activities;
+            newFilter.sort(dynamicSort("end_date"));
             sendReport(newFilter)
           }
         }
@@ -299,7 +300,21 @@ router.post("/harvested/:landid", (req, res, next) => {
     }
   }
 });
-
+function dynamicSort(property) {
+  var sortOrder = 1;
+  if (property[0] === "-") {
+    sortOrder = -1;
+    property = property.substr(1);
+  }
+  return function(a, b) {
+    /* next line works with strings and numbers,
+     * and you may want to customize it to your needs
+     */
+    var result =
+      a[property] < b[property] ? -1 : a[property] > b[property] ? 1 : 0;
+    return result * sortOrder;
+  };
+}
 function getMonday(d) {
   d = new Date(d);
   var day = d.getDay(),
